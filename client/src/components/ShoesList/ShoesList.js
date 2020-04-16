8a275efd5736e8168cc536fde82bf30684491025
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-04-16 19:28:43 +0300

    Adding sorting filter for shoes

--- client/src/components/ShoesList/ShoesList.js
+++ client/src/components/ShoesList/ShoesList.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import { StyledShoesList } from "./ShoesList-styles";
+import { Dropdown } from "semantic-ui-react";
 import { Pagination } from "@material-ui/lab";
 import { fetchShoesList } from "../../redux/actions/shoesActions";
 
 class ShoesList extends React.Component {
+  state = {
+    sortOptions: [
+      { key: "most-recent", value: "most-recent", text: "Most recent" },
+      { key: "high-to-low", value: "high-to-low", text: "Price: High-Low" },
+      { key: "low-to-high", value: "low-to-high", text: "Price: Low-High" }
+    ]
+  };
+
   componentDidMount = () => {
     this.props.fetchShoesList(2);
   };
@@ -14,6 +23,13 @@ class ShoesList extends React.Component {
 
     return (
       <StyledShoesList id="shoes-list">
+        <form id="filter-sort">
+          <Dropdown
+            options={this.state.sortOptions}
+            defaultValue={this.state.sortOptions[0].value}
+            selection
+          />
+        </form>
         <Pagination count={numOfPages} />
       </StyledShoesList>
     );
