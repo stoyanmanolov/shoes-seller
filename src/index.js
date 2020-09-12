a9bb93e56227b9fd0deffe2402ca455368bf7f08
Stoyan Manolov <stoyanmanolovvv@gmail.com>
2020-09-12 16:00:12 +0300

    Adding deployment information

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const shoesRouter = require("./routers/shoes");
 const authRouter = require("./routers/auth");
 const ordersRouter = require("./routers/orders");
+const path = require("path");
 require("./db/mongoose");
 
 if (process.env.NODE_ENV !== "production") {
@@ -21,3 +22,11 @@ app.use("/images", express.static(__dirname + "/images"));
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
